350935dd41db50f1ffe5578d6eb573de05c3e3dd
Aria Groult <ariagroult@luckycart.com>
2022-02-02 11:26:01 +0100

    Updated ignored folders policies

--- dist/utils/index.d.ts
+++
@@ -1,3 +0,0 @@
-declare function maxHist(C: number, row: number[]): [area: number, left: number, right: number];
-declare function maxRectangle(R: number, C: number, A: number[][]): [area: number, top: number, bottom: number, left: number, right: number];
-declare function main(): void;
