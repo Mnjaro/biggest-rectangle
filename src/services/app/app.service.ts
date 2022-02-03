c5332b4232fd19841f7b788f09ac163bba411743
Aria Groult <ariagroult@luckycart.com>
2022-02-03 11:16:50 +0100

    Updated app service to call the real utils

--- src/services/app/app.service.ts
+++ src/services/app/app.service.ts
@@ -1,10 +1,21 @@
 import { Injectable } from '@nestjs/common'
-import { Game } from '../../@types/game.types'
+import { BiggestRectangle } from 'src/@types/game.types'
 import { UpdateGameDTO } from '../../dtos/game.dto'
+import { BiggestRectangleUtils } from '../../utils/2d-matrix'
 
 @Injectable()
 export class AppService {
-    updateGame(updateGameDTO: UpdateGameDTO): Game {
-        return updateGameDTO.game
+
+    private biggestRectangleUtils: BiggestRectangleUtils
+
+    constructor() {
+        this.biggestRectangleUtils = new BiggestRectangleUtils()
+    }
+
+    updateGame(updateGameDTO: UpdateGameDTO): BiggestRectangle {
+        const rows = updateGameDTO.game.length
+        const cols = updateGameDTO.game[0].length
+
+        return this.biggestRectangleUtils.maxRectangle(rows, cols, updateGameDTO.game)
     }
 }
